b472a57fba55f213cf3a131b405e9869b016381b
AidaSofic <ssalihbegovic.aida@gmail.com>
2016-03-21 20:37:37 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scan = new Scanner (System.in);
+        System.out.print("");
+
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int sum = 0;
+
+        sum = a + b;
+
+    System.out.println(sum);
+
+    }
+}
